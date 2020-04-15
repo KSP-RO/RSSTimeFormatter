447c2f0b60f9bc32ba0eca81bec89081000489f9
Simon Meulenbeek <simon@simonmb.xyz>
2020-04-15 16:34:13 +0200

    Convert to PascalCase Convert spaces to tabs Convert most string.Format to string interpolation

--- RSSTimeFormatter/DTReplacer.cs
+++ RSSTimeFormatter/DTReplacer.cs
@@ -9,58 +9,58 @@ namespace RSSTimeFormatter
 		public static string GetUniqueStringFromUniqueNode(string name, string node)
 		{
 			var configs = GameDatabase.Instance.GetConfigs(node);
-            if (configs.Length == 1) {
-                ConfigNode config = configs[0].config;
-                var formats = config.GetValues(name);
-                if (formats.Length == 1) {
-                    return formats[0];
-                } else {
-                    Debug.LogError($"`{node}` configuration has multiple `{name}` entries, falling back to default");
-                }
-            } else {
-                Debug.LogError($"Multiple `{node}` configurations, falling back to default");
-            }
-            return null;
+			if (configs.Length == 1) {
+				ConfigNode config = configs[0].config;
+				var formats = config.GetValues(name);
+				if (formats.Length == 1) {
+					return formats[0];
+				} else {
+					Debug.LogError($"`{node}` configuration has multiple `{name}` entries, falling back to default");
+				}
+			} else {
+				Debug.LogError($"Multiple `{node}` configurations, falling back to default");
+			}
+			return null;
 		}
 
 		public void Start()
 		{
 			Debug.Log("Replacing DateTime formatter");
-            KSPUtil.dateTimeFormatter = new RealDateTimeFormatter(getDateFormat(), getEpoch());
+			KSPUtil.dateTimeFormatter = new RealDateTimeFormatter(GetDateFormat(), GetEpoch());
 		}
 
-        private string getDateFormat()
-        {
-            // Since Unity overrides the CurrentCulture, we cannot rely on it to
-            // format dates in a way that the user will understand, see
-            // https://github.com/KSP-RO/RSSTimeFormatter/issues/2.
-            // This default is an international standard, namely ISO 8601 extended
-            // format.  It is chosen (and was designed) to avoid ambiguities on the
-            // order of month and day that are inevitable with formats using
-            // slashes.
-            string format = GetUniqueStringFromUniqueNode("dateFormat", "RSSTimeFormatter");
-            if (format != null) {
-                try {
-                    string.Format("{0:" + format + "}", new DateTime(1957, 10, 04));
-                } catch (FormatException) {
-                    Debug.LogError($"Invalid date format {format} using default");
-                }
-                return format;
-            }
-            return "yyyy-MM-dd";
-        }
+		private string GetDateFormat()
+		{
+			// Since Unity overrides the CurrentCulture, we cannot rely on it to
+			// format dates in a way that the user will understand, see
+			// https://github.com/KSP-RO/RSSTimeFormatter/issues/2.
+			// This default is an international standard, namely ISO 8601 extended
+			// format.  It is chosen (and was designed) to avoid ambiguities on the
+			// order of month and day that are inevitable with formats using
+			// slashes.
+			string format = GetUniqueStringFromUniqueNode("dateFormat", "RSSTimeFormatter");
+			if (format != null) {
+				try {
+					string.Format("{0:" + format + "}", new DateTime(1957, 10, 04));
+				} catch (FormatException) {
+					Debug.LogError($"Invalid date format {format} using default");
+				}
+				return format;
+			}
+			return "yyyy-MM-dd";
+		}
 
-        private DateTime getEpoch()
-        {
-            DateTime result;
-            string customEpoch = GetUniqueStringFromUniqueNode("epoch", "RSSTimeFormatter");
-            if (customEpoch != null) {
-                if (DateTime.TryParse(customEpoch, out result)) {
-                    return result;                                        
-                }
-            }
-            return new DateTime(1951, 01, 01);
-        }
+		private DateTime GetEpoch()
+		{
+			DateTime result;
+			string customEpoch = GetUniqueStringFromUniqueNode("epoch", "RSSTimeFormatter");
+			if (customEpoch != null) {
+				if (DateTime.TryParse(customEpoch, out result)) {
+					return result;                                        
+				}
+			}
+			return new DateTime(1951, 01, 01);
+		}
 	}
 }
 
--- RSSTimeFormatter/RealDateTimeFormatter.cs
+++ RSSTimeFormatter/RealDateTimeFormatter.cs
@@ -6,287 +6,300 @@ using UnityEngine;
 
 namespace RSSTimeFormatter
 {
-    public class RealDateTimeFormatter : IDateTimeFormatter
-    {
-        private string dateFormat;
-        private DateTime epoch;
+	public class RealDateTimeFormatter : IDateTimeFormatter
+	{
+		private string dateFormat;
+		private DateTime epoch;
 
-        #region IDateTimeFormatter implementation
-        public string PrintTimeLong(double time)
-        {
-            // short-circuit if invalid time passed
-            if (IsInvalidTime(time))
-                return InvalidTimeStr(time);
-            TimeSpan span = TimeSpan.FromSeconds(time);
-            return spanAsLongFormDateString(span);
-        }
-        public string PrintTimeStamp(double time, bool days = false, bool years = false)
-        {
-            // short-circuit if invalid time passed
-            if (IsInvalidTime(time))
-                return InvalidTimeStr(time);
-            TimeSpan span = TimeSpan.FromSeconds(time);
-            return string.Format("{0}{1}"
-                , days ? string.Format("Day {0} - ", span.Days) : ""
-                , spanAs24HWithSeconds(span)
-            );
-        }
-        public string PrintTimeStampCompact(double time, bool days = false, bool years = false)
-        {
-            // short-circuit if invalid time passed
-            if (IsInvalidTime(time))
-                return InvalidTimeStr(time);
-            TimeSpan span = TimeSpan.FromSeconds(time);
-            int totalYears = (int)(span.TotalDays / 365);
-            return string.Format("{0}{1}{2}"
-                , years ? string.Format("{0}y, ", totalYears) : ""
-                , days ? string.Format("{0}d, ", span.Days) : ""
-                , spanAs24HWithSeconds(span)
-            );
-        }
-        public string PrintTime(double time, int valuesOfInterest, bool explicitPositive)
-        {
-            // This is a downright strange and confusing method but as I understand it
-            // what it is saying is give it the time in the following format:
-            // 1y, 1d, 1h, 1m, 1s
-            // But the param 'valuesOfInterest' is used to control how 'deep' it goes
-            // IOW a valueofInterest of say 3 would only give you hours, minutes, and seconds
-            // Why it isn't more straightforward is beyond me
-            // short-circuit if invalid time passed
-            if (IsInvalidTime(time))
-                return InvalidTimeStr(time);
-            bool isTimeNegative = time < 0;
-            time = Math.Abs(time);
-            TimeSpan span = TimeSpan.FromSeconds(time);
-            int years = (int)(span.TotalDays / 365);
-            return string.Format("{0}{1}{2}{3}{4}{5}"
-                , isTimeNegative ? "- " : (explicitPositive ? "+ " : "")
-                , years > 0 ? string.Format("{0}y", years) : ""
-                , (valuesOfInterest >= 1 && span.Days != 0) ? string.Format(", {0}d", span.Days) : ""
-                , (valuesOfInterest >= 2 && span.Hours != 0) ? string.Format(", {0}h", span.Hours) : ""
-                , (valuesOfInterest >= 3 && span.Minutes != 0) ? string.Format(", {0}m", span.Minutes) : ""
-                , valuesOfInterest >= 4 ? string.Format(", {0}s", span.Seconds) : ""
-            );
-        }
+		#region IDateTimeFormatter implementation
+
+		public string PrintTimeLong(double time)
+		{
+			// short-circuit if invalid time passed
+			if (IsInvalidTime(time))
+				return InvalidTimeStr(time);
+			TimeSpan span = TimeSpan.FromSeconds(time);
+			return SpanAsLongFormDateString(span);
+		}
+
+		public string PrintTimeStamp(double time, bool days = false, bool years = false)
+		{
+			// short-circuit if invalid time passed
+			if (IsInvalidTime(time))
+				return InvalidTimeStr(time);
+			TimeSpan span = TimeSpan.FromSeconds(time);
+			return string.Format("{0}{1}"
+				, days ? $"Day {span.Days} - " : ""
+				, SpanAs24HWithSeconds(span)
+			);
+		}
+
+		public string PrintTimeStampCompact(double time, bool days = false, bool years = false)
+		{
+			// short-circuit if invalid time passed
+			if (IsInvalidTime(time))
+				return InvalidTimeStr(time);
+			TimeSpan span = TimeSpan.FromSeconds(time);
+			int totalYears = (int)(span.TotalDays / 365);
+			return string.Format("{0}{1}{2}"
+				, years ? $"{totalYears}y, " : ""
+				, days ? $"{span.Days}d, " : ""
+				, SpanAs24HWithSeconds(span)
+			);
+		}
+
+		public string PrintTime(double time, int valuesOfInterest, bool explicitPositive)
+		{
+			// This is a downright strange and confusing method but as I understand it
+			// what it is saying is give it the time in the following format:
+			// 1y, 1d, 1h, 1m, 1s
+			// But the param 'valuesOfInterest' is used to control how 'deep' it goes
+			// IOW a valueofInterest of say 3 would only give you hours, minutes, and seconds
+			// Why it isn't more straightforward is beyond me
+			// short-circuit if invalid time passed
+			if (IsInvalidTime(time))
+				return InvalidTimeStr(time);
+			bool isTimeNegative = time < 0;
+			time = Math.Abs(time);
+			TimeSpan span = TimeSpan.FromSeconds(time);
+			int years = (int)(span.TotalDays / 365);
+			return string.Format("{0}{1}{2}{3}{4}{5}"
+				, isTimeNegative ? "- " : (explicitPositive ? "+ " : "")
+				, years > 0 ? $"{years}y" : ""
+				, (valuesOfInterest >= 1 && span.Days != 0) ? $", {span.Days}d" : ""
+				, (valuesOfInterest >= 2 && span.Hours != 0) ? $", {span.Hours}h" : ""
+				, (valuesOfInterest >= 3 && span.Minutes != 0) ? $", {span.Minutes}m": ""
+				, valuesOfInterest >= 4 ? $", {span.Seconds}s" : ""
+			);
+		}
 
         public string PrintTime(double time, int valuesOfInterest, bool explicitPositive, bool logEnglish)
-        {
-            return PrintTime(time, valuesOfInterest, explicitPositive);
-        }
+		{
+			return PrintTime(time, valuesOfInterest, explicitPositive);
+		}
 
-        public string PrintTimeCompact(double time, bool explicitPositive)
-        {
-            if (IsInvalidTime(time))
-                return InvalidTimeStr(time);
-            bool isTimeNegative = time < 0;
-            time = Math.Abs(time);
-            TimeSpan span = TimeSpan.FromSeconds(time);
-            return string.Format("{0}{1}{2}"
-                , isTimeNegative ? "- " : (explicitPositive ? "+ " : "")
-                , (span.Days != 0 ? span.Days.ToString() : "")
-                , spanAs24HWithSeconds(span)
-            );
-        }
-        public string PrintDateDelta(double time, bool includeTime, bool includeSeconds, bool useAbs)
-        {
-            if (IsInvalidTime(time))
-                return InvalidTimeStr(time);
-            time = useAbs ? Math.Abs(time) : time;
-            if (time == 0d)
-                return string.Format("0 {0}", includeTime ? (includeSeconds ? "seconds" : "minutes") : "days");
-            TimeSpan span = TimeSpan.FromSeconds(time);
-            return spanAsLongFormDateString(span, " ");
-        }
-        public string PrintDateDeltaCompact(double time, bool includeTime, bool includeSeconds, bool useAbs)
-        {
-            if (IsInvalidTime(time))
-                return InvalidTimeStr(time);
-            time = useAbs ? Math.Abs(time) : time;
-            if (time == 0d)
-                return string.Format("0{0}", includeTime ? (includeSeconds ? "s" : "m") : "d");
-            TimeSpan span = TimeSpan.FromSeconds(time);
-            return spanAsShortFormDateString(span, " ");
-        }
-        public string PrintDate(double time, bool includeTime, bool includeSeconds = false)
-        {
-            if (IsInvalidTime(time))
-                return InvalidTimeStr(time);
-            DateTime target = GetEpoch().AddSeconds(time);
-            return string.Format("{0:" + dateFormat + "}{1}"
-                , target
-                , includeTime ? (" " + dateTimeAs24H(target, includeSeconds)) : ""
-            );
-        }
-        public string PrintDateNew(double time, bool includeTime)
-        {
-            return PrintDate(time, includeTime, true);
-        }
+		public string PrintTimeCompact(double time, bool explicitPositive)
+		{
+			if (IsInvalidTime(time))
+				return InvalidTimeStr(time);
+			bool isTimeNegative = time < 0;
+			time = Math.Abs(time);
+			TimeSpan span = TimeSpan.FromSeconds(time);
+			return string.Format("{0}{1}{2}"
+				, isTimeNegative ? "- " : (explicitPositive ? "+ " : "")
+				, (span.Days != 0 ? span.Days.ToString() : "")
+				, SpanAs24HWithSeconds(span)
+			);
+		}
 
-        public string PrintDateCompact(double time, bool includeTime, bool includeSeconds = false)
-        {
-            if (IsInvalidTime(time))
-                return InvalidTimeStr(time);
+		public string PrintDateDelta(double time, bool includeTime, bool includeSeconds, bool useAbs)
+		{
+			if (IsInvalidTime(time))
+				return InvalidTimeStr(time);
+			time = useAbs ? Math.Abs(time) : time;
+			if (time == 0d)
+				return string.Format("0 {0}", includeTime ? (includeSeconds ? "seconds" : "minutes") : "days");
+			TimeSpan span = TimeSpan.FromSeconds(time);
+			return SpanAsLongFormDateString(span, " ");
+		}
 
-            DateTime target = GetEpoch().AddSeconds(time);
+		public string PrintDateDeltaCompact(double time, bool includeTime, bool includeSeconds, bool useAbs)
+		{
+			if (IsInvalidTime(time))
+				return InvalidTimeStr(time);
+			time = useAbs ? Math.Abs(time) : time;
+			if (time == 0d)
+				return string.Format("0{0}", includeTime ? (includeSeconds ? "s" : "m") : "d");
+			TimeSpan span = TimeSpan.FromSeconds(time);
+			return SpanAsShortFormDateString(span, " ");
+		}
 
-            return string.Format("{0}-{1}{2}"
-                , target.Year
-                , target.DayOfYear
-                , includeTime ? (" " + dateTimeAs24H(target, includeSeconds)) : ""
-            );
-        }
+		public string PrintDate(double time, bool includeTime, bool includeSeconds = false)
+		{
+			if (IsInvalidTime(time))
+				return InvalidTimeStr(time);
+			DateTime target = GetEpoch().AddSeconds(time);
+			return string.Format("{0:" + dateFormat + "}{1}"
+				, target
+				, includeTime ? $" {DateTimeAs24H(target, includeSeconds)}" : ""
+			);
+		}
 
-        public int Minute {
-            get {
-                return 60;
-            }
-        }
-        public int Hour {
-            get {
-                return 3600;
-            }
-        }
-        public int Day {
-            get {
-                return 86400;
-            }
-        }
-        public int Year {
-            get {
-                return 31536000;
-            }
-        }
-        #endregion
-        private string spanAs24H(TimeSpan span, bool includeSeconds)
-        {
-            if (includeSeconds)
-                return spanAs24HWithSeconds(span);
-            return spanAs24H(span);
-        }
-        private string spanAs24H(TimeSpan span)
-        {
-            return string.Format("{0:D2}:{1:D2}",
-                span.Hours,
-                span.Minutes);
-        }
-        private string spanAs24HWithSeconds(TimeSpan span)
-        {
-            return string.Format("{0:D2}:{1:D2}:{2:D2}",
-                span.Hours,
-                span.Minutes,
-                span.Seconds);
-        }
+		public string PrintDateNew(double time, bool includeTime)
+		{
+			return PrintDate(time, includeTime, true);
+		}
 
-        private string dateTimeAs24H(DateTime time, bool includeSeconds)
-        {
-            if (includeSeconds)
-                return dateTimeAs24HWithSeconds(time);
-            return dateTimeAs24H(time);
-        }
-        private string dateTimeAs24H(DateTime time)
-        {
-            return string.Format("{0:D2}:{1:D2}",
-                time.Hour,
-                time.Minute);
-        }
-        private string dateTimeAs24HWithSeconds(DateTime time)
-        {
-            return string.Format("{0:D2}:{1:D2}:{2:D2}",
-                time.Hour,
-                time.Minute,
-                time.Second);
-        }
+		public string PrintDateCompact(double time, bool includeTime, bool includeSeconds = false)
+		{
+			if (IsInvalidTime(time))
+				return InvalidTimeStr(time);
 
-        private string spanAsDateString(TimeSpan span, bool longForm, string seperator = ", ")
-        {
-            if (longForm) return spanAsLongFormDateString(span, seperator);
-            return spanAsShortFormDateString(span, seperator);
-        }
-        private string spanAsShortFormDateString(TimeSpan span, string seperator = ", ")
-        {
-            return string.Format("{1}d{0}{2}h{0}{3}m{0}{4}s"
-                , seperator
-                , span.Days
-                , span.Hours
-                , span.Minutes
-                , span.Seconds
-            );
-        }
-        private string spanAsLongFormDateString(TimeSpan span, string seperator = ", ")
-        {
-            return string.Format("{1}{2}{0}{3}{4}{0}{5}{6}{0}{7}{8}"
-                , seperator
-                , span.Days, span.Days == 1 ? "day" : "days"
-                , span.Hours, span.Hours == 1 ? "hour" : "hours"
-                , span.Minutes, span.Minutes == 1 ? "minute" : "minutes"
-                , span.Seconds, span.Seconds == 1 ? "second" : "seconds"
-            );
-        }
-        private string dateTimeAsDateString(DateTime time, bool longForm, string seperator = ", ")
-        {
-            if (longForm) return dateTimeAsLongFormDateString(time, seperator);
-            return dateTimeAsShortFormDateString(time, seperator);
-        }
-        private string dateTimeAsShortFormDateString(DateTime time, string seperator = ", ")
-        {
-            return string.Format("{1}d{0}{2}h{0}{3}m{0}{4}s"
-                , seperator
-                , time.Day
-                , time.Hour
-                , time.Minute
-                , time.Second
-            );
-        }
-        private string dateTimeAsLongFormDateString(DateTime time, string seperator = ", ")
-        {
-            return string.Format("{1}{2}{0}{3}{4}{0}{5}{6}{0}{7}{8}"
-                , seperator
-                , time.Day, time.Day == 1 ? "day" : "days"
-                , time.Hour, time.Hour == 1 ? "hour" : "hours"
-                , time.Minute, time.Minute == 1 ? "minute" : "minutes"
-                , time.Second, time.Second == 1 ? "second" : "seconds"
-            );
-        }
+			DateTime target = GetEpoch().AddSeconds(time);
 
-        protected bool IsInvalidTime(double time)
-        {
-            if (double.IsNaN(time) || double.IsPositiveInfinity(time) || double.IsNegativeInfinity(time))
-                return true;
-            else
-                return false;
-        }
-        protected string InvalidTimeStr(double time)
-        {
-            if (double.IsNaN(time)) {
-                return "NaN";
-            }
-            if (double.IsPositiveInfinity(time)) {
-                return "+Inf";
-            }
-            if (double.IsNegativeInfinity(time)) {
-                return "-Inf";
-            }
-            return null;
-        }
+			return string.Format("{0}-{1}{2}"
+				, target.Year
+				, target.DayOfYear
+				, includeTime ? $" {DateTimeAs24H(target, includeSeconds)}" : ""
+			);
+		}
 
-        protected DateTime DateFromUT(double time)
-        {
-            return GetEpoch().AddSeconds(time);
-        }
+		public int Minute {
+			get {
+				return 60;
+			}
+		}
 
-        protected DateTime GetEpoch()
-        {
-            return epoch;
-        }
+		public int Hour {
+			get {
+				return 3600;
+			}
+		}
 
-        public RealDateTimeFormatter()
-        {
-        }
+		public int Day {
+			get {
+				return 86400;
+			}
+		}
 
-        public RealDateTimeFormatter(string dateFormat, DateTime epoch)
-        {
-            this.dateFormat = dateFormat;
-            this.epoch = epoch;
-        }
-    }
+		public int Year {
+			get {
+				return 31536000;
+			}
+		}
+
+		#endregion
+
+		private string SpanAs24H(TimeSpan span, bool includeSeconds)
+		{
+			if (includeSeconds)
+				return SpanAs24HWithSeconds(span);
+			return SpanAs24H(span);
+		}
+
+		private string SpanAs24H(TimeSpan span)
+		{
+            return $"{span.Hours:D2}:{span.Minutes:D2}";
+		}
+
+		private string SpanAs24HWithSeconds(TimeSpan span)
+		{
+            return $"{span.Hours:D2}:{span.Minutes:D2}:{span.Seconds:D2}";
+		}
+
+		private string DateTimeAs24H(DateTime time, bool includeSeconds)
+		{
+			if (includeSeconds)
+				return DateTimeAs24HWithSeconds(time);
+			return DateTimeAs24H(time);
+		}
+
+		private string DateTimeAs24H(DateTime time)
+		{
+            return $"{time.Hour:D2}:{time.Minute:D2}";
+		}
+
+		private string DateTimeAs24HWithSeconds(DateTime time)
+		{
+			return $"{time.Hour:D2}:{time.Minute:D2}:{time.Second:D2}";
+		}
+
+		private string SpanAsDateString(TimeSpan span, bool longForm, string seperator = ", ")
+		{
+			if (longForm) return SpanAsLongFormDateString(span, seperator);
+			return SpanAsShortFormDateString(span, seperator);
+		}
+
+		private string SpanAsShortFormDateString(TimeSpan span, string seperator = ", ")
+		{
+			return string.Format("{1}d{0}{2}h{0}{3}m{0}{4}s"
+				, seperator
+				, span.Days
+				, span.Hours
+				, span.Minutes
+				, span.Seconds
+			);
+		}
+
+		private string SpanAsLongFormDateString(TimeSpan span, string seperator = ", ")
+		{
+			return string.Format("{1}{2}{0}{3}{4}{0}{5}{6}{0}{7}{8}"
+				, seperator
+				, span.Days, span.Days == 1 ? "day" : "days"
+				, span.Hours, span.Hours == 1 ? "hour" : "hours"
+				, span.Minutes, span.Minutes == 1 ? "minute" : "minutes"
+				, span.Seconds, span.Seconds == 1 ? "second" : "seconds"
+			);
+		}
+
+		private string DateTimeAsDateString(DateTime time, bool longForm, string seperator = ", ")
+		{
+			if (longForm) return DateTimeAsLongFormDateString(time, seperator);
+			return DateTimeAsShortFormDateString(time, seperator);
+		}
+
+		private string DateTimeAsShortFormDateString(DateTime time, string seperator = ", ")
+		{
+			return string.Format("{1}d{0}{2}h{0}{3}m{0}{4}s"
+				, seperator
+				, time.Day
+				, time.Hour
+				, time.Minute
+				, time.Second
+			);
+		}
+
+		private string DateTimeAsLongFormDateString(DateTime time, string seperator = ", ")
+		{
+			return string.Format("{1}{2}{0}{3}{4}{0}{5}{6}{0}{7}{8}"
+				, seperator
+				, time.Day, time.Day == 1 ? "day" : "days"
+				, time.Hour, time.Hour == 1 ? "hour" : "hours"
+				, time.Minute, time.Minute == 1 ? "minute" : "minutes"
+				, time.Second, time.Second == 1 ? "second" : "seconds"
+			);
+		}
+
+		protected bool IsInvalidTime(double time)
+		{
+			if (double.IsNaN(time) || double.IsPositiveInfinity(time) || double.IsNegativeInfinity(time))
+				return true;
+			else
+				return false;
+		}
+
+		protected string InvalidTimeStr(double time)
+		{
+			if (double.IsNaN(time)) {
+				return "NaN";
+			}
+			if (double.IsPositiveInfinity(time)) {
+				return "+Inf";
+			}
+			if (double.IsNegativeInfinity(time)) {
+				return "-Inf";
+			}
+			return null;
+		}
+
+		protected DateTime DateFromUT(double time)
+		{
+			return GetEpoch().AddSeconds(time);
+		}
+
+		protected DateTime GetEpoch()
+		{
+			return epoch;
+		}
+
+		public RealDateTimeFormatter()
+		{
+		}
+
+		public RealDateTimeFormatter(string dateFormat, DateTime epoch)
+		{
+			this.dateFormat = dateFormat;
+			this.epoch = epoch;
+		}
+	}
 }

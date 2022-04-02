c91bbbd0eef3bc028e147a2f7275b7c553cd3f67
Robin Leroy <egg.robin.leroy@gmail.com>
2022-04-02 23:22:02 +0200

    Use CLDR medium dates and time by default, and format in the correct language.

--- RSSTimeFormatter/DTReplacer.cs
+++ RSSTimeFormatter/DTReplacer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 namespace RSSTimeFormatter
@@ -23,7 +24,17 @@ namespace RSSTimeFormatter
                 }
                 else if (formats.Length == 1)
                 {
-                    return formats[0];
+                    // Unescaping similar to that performed in KSP localization
+                    // strings (because we need curly braces), though contrary
+                    // to KSP we reject unpaired surrogates and instead accept
+                    // codepoints outside the BMP with the \U syntax.
+                    return new Regex(
+                        @"\\(?:u([0-9A-F]{4})|U([0-9A-F]{5}))").Replace(
+                        formats[0].Replace("｢", "{").Replace("｣", "}"),
+                        match => char.ConvertFromUtf32(
+                            int.Parse(
+                                match.Value.Substring(2),
+                                System.Globalization.NumberStyles.HexNumber)));
                 }
             }
             return null;
@@ -31,18 +42,15 @@ namespace RSSTimeFormatter
 
         public void Start()
         {
-            // Since Unity overrides the CurrentCulture, we cannot rely on it to
-            // format dates in a way that the user will understand, see
-            // https://github.com/KSP-RO/RSSTimeFormatter/issues/2.
-            // This default is an international standard, namely ISO 8601 extended
-            // format.  It is chosen (and was designed) to avoid ambiguities on the
-            // order of month and day that are inevitable with formats using
-            // slashes.
-            string dateFormat = "yyyy-MM-dd";
+            string dateFormat = KSP.Localization.Localizer.GetStringByTag("#RSSTimeFormatter_CLDR_mediumDate");
+            string timeFormat = KSP.Localization.Localizer.GetStringByTag("#RSSTimeFormatter_CLDR_mediumTime");
+            string dateTimeFormat = KSP.Localization.Localizer.GetStringByTag("#RSSTimeFormatter_CLDR_mediumDateTime");
             string customDateFormat = GetUniqueStringFromUniqueNode("dateFormat", "RSSTimeFormatter");
+            string customTimeFormat = GetUniqueStringFromUniqueNode("timeFormat", "RSSTimeFormatter");
+            string customDateTimeFormat = GetUniqueStringFromUniqueNode("dateTimeFormat", "RSSTimeFormatter");
+            // Validate any custom patterns.
             if (customDateFormat != null)
             {
-                // Validate the format string.
                 try
                 {
                     string.Format("{0:" + customDateFormat + "}", new DateTime(1957, 10, 04));
@@ -53,6 +61,30 @@ namespace RSSTimeFormatter
                     Debug.LogError("[RSSDT] Invalid date format " + customDateFormat);
                 }
             }
+            if (customTimeFormat != null)
+            {
+                try
+                {
+                    string.Format("{0:" + customTimeFormat + "}", new DateTime(1957, 10, 04));
+                    timeFormat = customTimeFormat;
+                }
+                catch (FormatException)
+                {
+                    Debug.LogError("[RSSDT] Invalid time format " + customTimeFormat);
+                }
+            }
+            if (customDateTimeFormat != null)
+            {
+                try
+                {
+                    string.Format(customDateTimeFormat, "time", "date");
+                    dateTimeFormat = customDateTimeFormat;
+                }
+                catch (FormatException)
+                {
+                    Debug.LogError("[RSSDT] Invalid date and time combiner format " + customDateTimeFormat);
+                }
+            }
 
             DateTime epoch = new DateTime(1951, 01, 01);
             string customEpoch = GetUniqueStringFromUniqueNode("epoch", "RSSTimeFormatter");
@@ -61,7 +93,7 @@ namespace RSSTimeFormatter
                 Debug.LogError("[RSSDT] Invalid epoch " + customEpoch);
             }
 
-            KSPUtil.dateTimeFormatter = new RealDateTimeFormatter(dateFormat, epoch);
+            KSPUtil.dateTimeFormatter = new RealDateTimeFormatter(dateFormat, timeFormat, dateTimeFormat, epoch);
         }
     }
 }
